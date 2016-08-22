694b8da610bd751622576ad40b1e4babde5089f8
Menglong TAN <tanmenglong@gmail.com>
2016-08-22 21:19:02 +0800

    add easemob_client

---
+++ src/easemob_client.cc
@@ -0,0 +1,97 @@
+/* -*- encoding: utf-8; indent-tabs-mode: nil -*- */
+
+/***************************************************************
+ *
+ * Copyright (c) 2016, Menglong TAN <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the Apache Licence
+ *
+ **************************************************************/
+
+/**
+ * 
+ *
+ * @file easemob_client.cc
+ * @author Menglong TAN <tanmenglong@gmail.com>
+ * @date Sun Aug 21 20:53:57 2016
+ *
+ **/
+
+#include "easemob_client.h"
+
+#include <emclient.h>
+#include <emchatmanager_interface.h>
+#include <message/emmessage.h>
+#include <message/emtextmessagebody.h>
+
+using namespace std;
+using namespace easemob;
+
+EasemobClient::EasemobClient() : is_init_(false), is_login_(false) {}
+
+EasemobClient::~EasemobClient() {
+    client_->getChatManager().removeListener(this);
+}
+
+int EasemobClient::Init(const string &work_path, const string &appkey) {
+    EMChatConfigsPtr config = EMChatConfigsPtr(new EMChatConfigs(work_path, work_path, appkey));
+    config->setEnableConsoleLog(false);
+    client_ = EMClient::create(config);
+    client_->getChatManager().addListener(this);
+    is_init_ = true;
+    return 0;
+}
+
+void EasemobClient::SetReceiveCallback(ReceiveCallback callback) {
+    this->recv_callback_ = callback;
+}
+
+int EasemobClient::Login(const string &username, const string &password) {
+    if (!is_init_) {
+        return 0;
+    }
+    EMErrorPtr result = client_->login(username, password);
+    if (result->mErrorCode != EMError::EM_NO_ERROR) {
+        cout << result->mDescription << endl;
+    }
+    is_login_ = true;
+    login_user_ = username;
+    return 0;
+}
+
+int EasemobClient::Logout() {
+    if (!is_login_) {
+        return 1;
+    }
+    client_->logout();
+    is_login_ = false;
+    return 0;
+}
+
+int EasemobClient::SendMessage(const string &to, const string message) {
+    if (!is_init_ || !is_login_) {
+        return 1;
+    }
+    EMTextMessageBodyPtr body = EMTextMessageBodyPtr(new EMTextMessageBody(message));
+    EMMessagePtr msg = EMMessage::createSendMessage(login_user_, to, body);
+    client_->getChatManager().sendMessage(msg);
+    return 1;
+}
+
+void EasemobClient::onReceiveMessages(const EMMessageList &messages) {
+    list<string> messageList;
+
+    if (messages.size() > 0) {
+        for(auto m : messages) {
+            if (m->bodies().size() > 0 && m->bodies()[0]->type() == EMMessageBody::TEXT) {
+                string msg = static_cast<EMTextMessageBody*>(m->bodies()[0].get())->text();
+                messageList.push_back(msg);
+            }
+        }
+    }
+
+    recv_callback_(messageList);
+}
+
+/* vim: set expandtab shiftwidth=2 tabstop=2: */
---
+++ src/easemob_client.h
@@ -0,0 +1,60 @@
+/* -*- encoding: utf-8; indent-tabs-mode: nil -*- */
+
+/***************************************************************
+ *
+ * Copyright (c) 2016, Menglong TAN <tanmenglong@gmail.com>
+ *
+ * This program is free software; you can redistribute it
+ * and/or modify it under the terms of the GPL licence
+ *
+ **************************************************************/
+
+#ifndef _EASEMOB_CLIENT_H_
+#define _EASEMOB_CLIENT_H_
+
+/**
+ * 
+ *
+ * @file easemob_client.h
+ * @author Menglong TAN <tanmenglong@gmail.com>
+ * @date Sun Aug 21 20:44:24 2016
+ *
+ **/
+
+#include <string>
+#include <list>
+
+#include <emclient.h>
+#include <emchatmanager_listener.h>
+#include <emcallback.h>
+
+typedef void (*ReceiveCallback)(const std::list<std::string> messages);
+
+class EasemobClient : public easemob::EMChatManagerListener {
+protected:
+    ReceiveCallback recv_callback_;
+
+    bool is_init_;
+    bool is_login_;
+    std::string login_user_;
+
+    easemob::EMClient *client_;
+
+public:
+    EasemobClient();
+    virtual ~EasemobClient();
+
+    virtual int Init(const std::string &work_path, const std::string &appkey);
+    virtual void SetReceiveCallback(ReceiveCallback callback);
+
+    virtual int Login(const std::string &username, const std::string &password);
+    virtual int Logout();
+    virtual int SendMessage(const std::string &to, const std::string message);
+
+    // EMChatManagerListener calback
+    virtual void onReceiveMessages(const easemob::EMMessageList &messages);
+};
+
+#endif /* _EASEMOB_CLIENT_H_ */
+
+/* vim: set expandtab shiftwidth=2 tabstop=2: */
